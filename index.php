99dc7118151dbcee7828cbe2f1d7fd87a13fdd1b
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-19 15:29:56 +0100

    Creato form

--- index.php
+++ index.php
@@ -1,11 +1,61 @@
+<?php
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
+
 <head>
     <meta charset="UTF-8">
+
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <title>Generate password</title>
 </head>
+
 <body>
-    
+
+<div class="container p-5">
+
+    <form>
+        <div class="mb-3">
+            <label for="userChoice" class="form-label">Inserisci il numero per poter generare la tua password</label>
+            <input type="number" class="form-control" id="userChoice" name="userChoice" aria-describedby="number" min="6" max="12">
+            <div id="emailHelp" class="form-text">Inserisci un numero da 6 a 12.</div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
