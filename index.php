5c1498977dbc205febb2d4449fc1c2f6ca5237e4
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-19 17:14:18 +0100

    Milestone 2 completata

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
 
-$userChoice = isset($_GET['userChoice']) ? $_GET['userChoice'] : null;
 
-$password = '';
-
-function getGeneratePassword($length)
-{
-    $randomPassword = 'abcdefghilmnopqrstuvzABCDEFGHILMNOPQRSTUVZ123456789./!*_';
-    $password = '';
-    $passwordLength = strlen($randomPassword) - 1;
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $randomPassword[rand(0, $passwordLength)];
-    }
-    return $password;
-}
-
-if ($userChoice) {
-    $password = getGeneratePassword($userChoice);
-}
+include __DIR__ . './functions.php'
 
 ?>
 
